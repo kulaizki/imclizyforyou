47c275eb996d886c9ef0f11187b53a5f5e37dadc
kulaizki <kulaizke@gmail.com>
2024-11-01 17:28:45 +0800

    refactor: format code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,6 @@ export default function RootLayout({
 }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="font-sans antialiased">
         {children}
       </body>

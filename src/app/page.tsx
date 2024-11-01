c8ca1a4befe6b7cfe188e8ed0baca921b65e31d3
kulaizki <kulaizke@gmail.com>
2024-11-01 23:12:44 +0800

    style: add tint to background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#fef1cf] gap-y-20">
       <video
         className="w-full h-screen object-cover"
         autoPlay
@@ -12,20 +12,20 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
 
-      <div className="relative z-10 text-center py-12 px-4">
+      <div className="flex flex-col z-10 text-center px-4 gap-6">
         <h1 className="text-7xl font-wedding text-gray-800 tracking-wide">
           Clive and Zyra&apos;s Wedding
         </h1>
-        <p className="text-xl font-body text-gray-600 mt-4 leading-7">
-          Celebrate this special day with us
+        <p className="text-xl font-body text-gray-700 leading-7">
+          Celebrate this special day with us!
         </p>
       </div>
 
-      <div className="relative z-10 text-center py-12 max-w-xl px-4">
+      <div className="flex flex-col z-10 text-center px-4 gap-6 max-w-2xl">
         <h1 className="text-5xl font-wedding font-semibold text-gray-800 tracking-wide">
           Venue
         </h1>
-        <p className="text-xl font-body text-gray-600 mt-4 leading-7">
+        <p className="text-xl font-body text-gray-700 leading-7">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
           reprehenderit accusantium minima eum porro laudantium dolor ipsum?
           Tempora, corporis laboriosam, repellendus eligendi ut iusto sunt optio
@@ -33,11 +33,11 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="relative z-10 text-center py-12 max-w-xl px-4">
+      <div className="flex flex-col z-10 text-center px-4 gap-6 max-w-2xl">
         <h1 className="text-5xl font-wedding font-semibold text-gray-800 tracking-wide">
           Attire
         </h1>
-        <p className="text-xl font-body text-gray-600 mt-4 leading-7">
+        <p className="text-xl font-body text-gray-700 leading-7">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
           reprehenderit accusantium minima eum porro laudantium dolor ipsum?
           Tempora, corporis laboriosam, repellendus eligendi ut iusto sunt optio
@@ -45,11 +45,11 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="relative z-10 text-center py-12 max-w-xl px-4">
+      <div className="flex flex-col z-10 text-center px-4 gap-6 max-w-2xl">
         <h1 className="text-5xl font-wedding font-semibold text-gray-800 tracking-wide">
           Join us
         </h1>
-        <p className="text-xl font-body text-gray-600 mt-4 leading-7">
+        <p className="text-xl font-body text-gray-700 leading-7">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
           reprehenderit accusantium minima eum porro laudantium dolor ipsum?
           Tempora, corporis laboriosam, repellendus eligendi ut iusto sunt optio
@@ -60,12 +60,11 @@ export default function Home() {
       <iframe
         title="RSVP Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLSduD45efj0H9cTl_ClTztjaJoQM-626YssPMN7PaAI26U1-jA/viewform?embedded=true"
-        className="w-full h-96 md:h-96 lg:h-[474px] border-0 mb-10"
+        className="w-full h-[580px] border-0"
         allowFullScreen
       >
         Loading…
       </iframe>
-
     </div>
   );
 }

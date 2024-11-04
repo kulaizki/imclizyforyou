dde10451829040ff5db888e1c7cbddfe615d709d
kulaizki <kulaizke@gmail.com>
2024-11-05 03:56:04 +0800

    feat: add links to CopyrightFooter

--- src/components/CopyrightFooter.tsx
+++ src/components/CopyrightFooter.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function CopyrightFooter() {
   return (
     <footer className="text-center text-slate-200 py-4 bg-slate-900">
-      &copy; {new Date().getFullYear()} kulaizki & tomazuu. All rights reserved.
+      &copy; {new Date().getFullYear()}{" "}
+      <Link href="https://www.linkedin.com/in/kulaizki/" target="_blank" rel="noopener noreferrer" className="font-semibold">
+        kulaizki
+      </Link>{" "}
+      &{" "}
+      <Link href="https://tomazuu.netlify.app/" target="_blank" rel="noopener noreferrer" className="font-semibold">
+        tomazuu
+      </Link>
+      . All rights reserved.
     </footer>
   );
 }

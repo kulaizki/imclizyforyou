478defa705dda14cbbb3a098c89cdaf3e9f13993
kulaizki <kulaizke@gmail.com>
2024-11-09 15:57:07 +0800

    refactor: correct date

--- src/components/Venue.tsx
+++ src/components/Venue.tsx
@@ -3,7 +3,7 @@ export default function Venue() { return (
     <div className="flex flex-col gap-16 bg-[#faf5e7] bg-opacity-80 rounded-lg shadow-lg p-20 max-w-5xl w-full">
       <div className="text-center max-w-4xl">
         <div className="flex flex-col gap-4 text-lg sm:text-xl select-none text-gray-800">
-          <p className="uppercase">December 28, 2014 1:30 PM, Saturday</p>
+          <p className="uppercase">December 28, 2024 1:30 PM, Saturday</p>
           <p className="uppercase">Sacred Heart of Jesus Parish, Tacloban City</p>
         </div>
       </div>
